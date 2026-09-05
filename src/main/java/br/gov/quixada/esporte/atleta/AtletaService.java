2258ed2c5ea9f3da28dedb7685121266207ced49
Michael Barbosa <michael@gmail.com>
2026-09-05 10:57:23 -0300

    feat: correcao implementacao AtletaService

--- src/main/java/br/gov/quixada/esporte/atleta/AtletaService.java
+++ src/main/java/br/gov/quixada/esporte/atleta/AtletaService.java
@@ -2,8 +2,11 @@ package br.gov.quixada.esporte.atleta;
 
 import br.gov.quixada.esporte.exceptions.AtletaNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import javax.management.InstanceAlreadyExistsException;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +20,10 @@ public class AtletaService {
     }
 
     public Atleta save(Atleta atleta) {
+        if(atleta.getCpf() == null || atleta.getCpf().isEmpty()){
+            throw new IllegalArgumentException("CPF é obrigatório");
+        }
+
         return repository.save(atleta);
     }
 
@@ -25,13 +32,20 @@ public class AtletaService {
         repository.delete(atleta);
     }
 
-    public void update(Atleta atletaToUpdate) {
-        assertAtletaExist(atletaToUpdate);
-        repository.save(atletaToUpdate);
+    public void update(Long id, Atleta atletaParaAtualizar) {
+        Atleta atletaExistente = findByIdOrThrowNotFound(id);
+
+        atletaExistente.setNomeCompleto(atletaParaAtualizar.getNomeCompleto());
+        atletaExistente.setTelefone(atletaParaAtualizar.getTelefone());
+        atletaExistente.setDataNascimento(atletaParaAtualizar.getDataNascimento());
+        atletaExistente.setEndereco(atletaParaAtualizar.getEndereco());
+        atletaExistente.setAtivo(atletaParaAtualizar.isAtivo());
+        atletaExistente.setSexo(atletaParaAtualizar.getSexo());
+
+        repository.save(atletaExistente);
     }
 
-    public void assertAtletaExist(Atleta atleta) {
+    private void assertAtletaExist(Atleta atleta) {
         findByIdOrThrowNotFound(atleta.getId());
     }
-
 }
